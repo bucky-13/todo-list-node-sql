79a17c73c2eb2f644b255af087337bc957c33b31
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-02-13 16:29:40 +0100

    added route for login + check to avoid double user names or emails

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,6 @@ router.get('/', function (req, res, next) {
       if (err) {
         console.log(err);
       }
-      console.log('result', result);
       res.send(result);
     });
   });
@@ -39,30 +38,68 @@ router.get('/:id', function (req, res, next) {
       if (err) {
         console.log(err);
       }
-      console.log('result', result);
       res.send(result);
     });
   });
 });
 
+// create new user
 router.post('/', function (req, res, next) {
   req.app.locals.con.connect(function (err) {
     if (err) {
       console.log(err);
     }
 
-    let saveName = req.body.saveName;
-    let saveEmail = req.body.saveEmail;
+    let userName = req.body.userName;
+    let userEmail = req.body.userEmail;
+
+    let firstSql = `SELECT * FROM users WHERE userName="${userName}" OR userEmail="${userEmail}"`;
 
     // POST new user into database
-    let sql = `INSERT INTO users (userName, userEmail) VALUES ("${saveName}", "${saveEmail}")`;
+    let secondSql = `INSERT INTO users (userName, userEmail) VALUES ("${userName}", "${userEmail}")`;
+
+    req.app.locals.con.query(firstSql, function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+      if (!result[0]) {
+        console.log('user can be created');
+        req.app.locals.con.query(secondSql, function (err, result) {
+          if (err) {
+            console.log(err);
+          }
+          res.send(result);
+        });
+      } else {
+        res.status(409).json({ message: 'user name or email already exists' });
+      }
+    });
+  });
+});
+
+router.post('/login', (req, res, next) => {
+  req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    let userName = req.body.userName;
+    let userEmail = req.body.userEmail;
+
+    let sql = `SELECT userId FROM users WHERE userName="${userName}" AND userEmail="${userEmail}"`;
 
     req.app.locals.con.query(sql, function (err, result) {
       if (err) {
         console.log(err);
       }
-      console.log('result', result);
-      res.send(result);
+
+      if (!result[0]) {
+        res.status(401).json({
+          message: 'Login failed, user name or email does not exist.',
+        });
+      } else {
+        res.send(result);
+      }
     });
   });
 });
@@ -83,7 +120,6 @@ router.put('/', (req, res, next) => {
       if (err) {
         console.log(err);
       }
-      console.log('result', result);
       res.send(result);
     });
   });
@@ -95,15 +131,14 @@ router.delete('/', (req, res, next) => {
       console.log(err);
     }
 
-    let id = Number(req.body.findId);
+    let userId = Number(req.body.userId);
 
-    let sql = `DELETE FROM users WHERE id=${id}`;
+    let sql = `DELETE FROM users WHERE userId=${userId}`;
 
     req.app.locals.con.query(sql, function (err, result) {
       if (err) {
         console.log(err);
       }
-      console.log('result', result);
       res.send(result);
     });
   });
